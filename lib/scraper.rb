cf679497ffb0858966ff0d72ad3f7946db9c2a0f
Tom White <eugene.white@gmail.com>
2019-06-20 13:47:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,18 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page
+    doc = Nokogiri::HTML(open())
+  end 
+  
+  def get_courses
+    
+  end
+
+  def make_courses
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
+    end
     end
   end
   
